c700a1bd6faff5b0997a2844db11c0d768e1d308
Baconsizzle1738 <zhengdundun@gmail.com>
2022-09-24 21:04:25 -0500

    redid some player movement stuff

--- Levels/FirstLevel.gd
+++ Levels/FirstLevel.gd
@@ -6,6 +6,8 @@ onready var target = $Falling/Bounce/Player.global_position;
 var pos = Vector2(0,0);
 var playerMoving = false;
 var dir = "";
+
+const PLAYER = preload("res://Pieces/Entities/PlayerPawn.tscn");
 #move to player
 #var vol = Vector2();
 
@@ -15,23 +17,23 @@ func _ready():
 	$Bounce.play("Bouncy");
 
 func _input(event):
-	if !cutscene and !playerMoving:
+	if !cutscene and !playerMoving and playerTurn:
 		#print(event.to_string());
 		if event.is_action_pressed("click"):
 			print("CLICK");
 			#loop thru all the possible moves
-			for hit in $Falling/Bounce/Player/PossibleMoves.get_children():
+			for hit in $Player/PossibleMoves.get_children():
 				#print($Falling/Bounce/Player/PossibleMoves.get_children());
 				#check to see if mouse is in hitbox, then set the target location of movement to the position selected
 				#print(hit.getRect())
 				if hit.getRect().has_point(get_global_mouse_position()):
-					pos = $Falling/Bounce/Player.global_position;
+					pos = $Player.global_position;
 					print(pos);
 					target = hit.global_position;
 					#print(hit.get_name());
 					dir = hit.get_name();
 					#speed is porportioal to the distance
-					$Falling/Bounce/Player.vol = pos.direction_to(target) * pos.distance_to(target);
+					$Player.vol = pos.direction_to(target) * pos.distance_to(target);
 					playerMoving = true;
 					break;
 		#target = get_global_mouse_position();
@@ -40,10 +42,10 @@ func _input(event):
 func _physics_process(delta):
 	
 	if playerMoving:
-		if $Falling/Bounce/Player.global_position.distance_to(target) <= 5:
-			$Falling/Bounce/Player.vol = Vector2(0,0);
+		if $Player.global_position.distance_to(target) <= 3:
+			$Player.vol = Vector2(0,0);
 			
-			$Falling/Bounce/Player.global_position = target;
+			$Player.global_position = target;
 			#offset bc its gonna be off by 5 pixels
 			#if dir == "Up":
 				#$Falling/Bounce/Player.global_position.y -= $Falling/Bounce/Player.global_position.distance_to(target);
@@ -71,5 +73,14 @@ func _physics_process(delta):
 	
 	
 func _on_Bounce_animation_finished(anim_name):
+	
+	$Falling/Bounce/Player.queue_free();
+	var player = PLAYER.instance();
+	player.global_position.x = 480;
+	player.global_position.y = 416;
+	player.name = "Player";
+	add_child(player);
+	
+	
 	cutscene = false;
 	
